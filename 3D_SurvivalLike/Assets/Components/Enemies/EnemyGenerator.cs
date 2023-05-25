1f32d1c473ecfd6fb818c9d06404e02e6c3818d0
Batyr Bodaubay <batyr.bodaubay@nu.edu.kz>
2023-05-25 19:03:45 +0600

    Enemy Generator
    Starting to implement eneme generation

--- 3D_SurvivalLike/Assets/Components/Enemies/EnemyGenerator.cs
+++ 3D_SurvivalLike/Assets/Components/Enemies/EnemyGenerator.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
+    [SerializeField] private GameObject[] _enemyPrefabs;
+    [SerializeField] private float _spawnRate = 5f;
+    [SerializeField] private float _spawnRadius = 15f;
+    [SerializeField] private int _maxEnemies = 10;
+    [SerializeField] private int _enemyIndex = 0;
+    private Transform _player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        InvokeRepeating("SpawnBanchOfEnemies", 0f, _spawnRate);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    Vector3 RandomPositionAroundPlayer()
     {
-        
+        Vector3 randomPos = Random.insideUnitSphere * _spawnRadius;
+        randomPos += _player.position;
+
+        return new Vector3(randomPos.x, 0f, randomPos.z);    
+    }
+
+    public void SpawnEnemy(int enemyIndex)
+    {
+        Vector3 spawnPos = RandomPositionAroundPlayer();
+        GameObject enemy = Instantiate(_enemyPrefabs[enemyIndex], spawnPos, Quaternion.identity);
+        enemy.transform.LookAt(_player);
+    }
+
+    public void SpawnBanchOfEnemies()
+    {
+        int randomNum = Random.Range(5, _maxEnemies);
+        for (int i = 0; i < randomNum; i++)
+        {
+            SpawnEnemy(_enemyIndex);
+        }
     }
 }
